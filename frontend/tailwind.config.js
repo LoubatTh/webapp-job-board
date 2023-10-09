210a85d46ed397e14d558654ad9efa4074499e8c
LoubatTh <thomasloubat1@gmail.com>
2023-10-09 14:46:43 +0200

    add: front boilerplate

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+// import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx, html}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    // nextui()
+  ],
+};

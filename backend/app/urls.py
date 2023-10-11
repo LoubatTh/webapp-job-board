51bf25da0d0e070b88f313e713b566e4ee2ba908
TalalBoni <talal.bounaama@epitech.eu>
2023-10-11 13:26:02 +0200

    ControlleurOKKK

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import hello, AdvertisementController
+from .views import hello, AdvertisementController, CompanyController
 
 urlpatterns = [
     path("hello", hello, name="hello"),
     path("advertisement", AdvertisementController.as_view(), name="advertisement"),
+    path("company", CompanyController.as_view(), name="advertisement"),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -5,7 +5,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializer import AdvertisementSerializer
+from .serializer import CompanySerializer
+from .serializer import ApplicationSerializer
+from .serializer import UserSerializer
+from .models import User
 from .models import Advertisement
+from .models import Company
+from .models import Application
+
 
 
 # Create your views here.
@@ -51,3 +58,126 @@ class AdvertisementController(APIView):
         advertisement = get_object_or_404(Advertisement, pk=pk)
         advertisement.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+class CompanyController(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, pk=None):
+        if pk:
+            company = get_object_or_404(Company, pk=pk)
+            serializer = CompanySerializer(
+                company, context={"request": request}
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            company = Company.objects.all()
+            serializer = CompanySerializer(company, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )   
+
+    def put(self, request, pk=None):
+        company = get_object_or_404(Company, pk=pk)
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )     
+    
+    def delete(self, request, pk=None):
+        company = get_object_or_404(Company, pk=pk)
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ApplicationController(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, pk=None):
+        if pk:
+            application = get_object_or_404(Application, pk=pk)
+            serializer = ApplicationSerializer(
+            application, context={"request": request}
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            application = Application.objects.all()
+            serializer = ApplicationSerializer(application, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def post(self, request):
+        serializer = ApplicationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )   
+    
+    def put(self, request, pk=None):
+        application = get_object_or_404(Application, pk=pk)
+        serializer = CompanySerializer(application, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )    
+    
+    def delete(self, request, pk=None):
+        application = get_object_or_404(Application, pk=pk)
+        application.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserController(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, pk=None):
+        if pk:
+            user = get_object_or_404(User, pk=pk)
+            serializer = UserSerializer(
+                user, context={"request": request}
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            user = User.objects.all()
+            serializer = UserSerializer(user, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    def put(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    def delete(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+

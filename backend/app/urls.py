6beb47b916ea10a266960a23a4c74f46225df9fe
TalalBoni <talal.bounaama@epitech.eu>
2023-10-11 17:24:58 +0200

    modifurls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import hello, AdvertisementController, CompanyController
+from .views import hello, AdvertisementController, CompanyController , ApplicationController
 
 urlpatterns = [
     path("hello", hello, name="hello"),
-    path("advertisement", AdvertisementController.as_view(), name="advertisement"),
-    path("company", CompanyController.as_view(), name="advertisement"),
+    path("advertisement/<int:pk>", AdvertisementController.as_view(), name="advertisement"),
+    path('company', CompanyController.as_view(), name="company"),
+    path('company/<int:pk>', CompanyController.as_view(), name="company"),
+    path('application/<int:pk>', ApplicationController.as_view(), name="application"),
+    path('application', ApplicationController.as_view(), name="application"),
 ]
